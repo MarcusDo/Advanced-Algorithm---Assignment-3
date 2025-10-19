eb3054c96c298eb134150d9048f62e104cf47341
NhatQuangDo <nquang.work@gmail.com>
2025-10-19 14:14:47 +1100

    Deterministic Select HPP file, with 2 main functions for answering the problem (generally it becomes the find kth smallest element problem, so both were implemented)

---
+++ deterministic_select.hpp
@@ -0,0 +1,8 @@
+#ifndef DETERMINISTIC_SELECT_HPP_
+#define DETERMINISTIC_SELECT_HPP_
+#include <vector>
+
+int getKthSmallest (const std::vector<int>& arr, int k);
+double getMedian (const std::vector<int>& arr );
+
+#endif
